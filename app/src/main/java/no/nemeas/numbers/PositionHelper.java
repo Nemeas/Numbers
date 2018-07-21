a767fed8545e5d1a14d51c1c196fbafec37d72b4
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-07-21 22:56:31 +0200

    implements the no-overlap logic in the positionhelper

--- app/src/main/java/no/nemeas/numbers/PositionHelper.java
+++ app/src/main/java/no/nemeas/numbers/PositionHelper.java
@@ -24,8 +24,24 @@ public class PositionHelper {
     }
 
     private static boolean isClear(Position[] positions, int x, int y, int size, int margin) {
-        // TODO - implement
+        for (Position position : positions) {
+            if (position != null && !isClear(position, x, y, size, margin)) return false;
+        }
+
         return true;
+    }
+
+    private static boolean isClear(Position position, int x, int y, int size, int margin) {
+        Position l1 = new Position(position.x - margin, position.y - margin);
+        Position r1 = new Position(position.x + size + margin, position.y + size + margin);
+        Position l2 = new Position(x, y);
+        Position r2 = new Position(x + size, y + size);
+
+        return !doOverlap(l1, r1, l2, r2);
+    }
+
+    private static boolean doOverlap(Position a1, Position a2, Position b1, Position b2) {
+        return a1.x < b2.x && a2.x > b1.x && a1.y < b2.y && a2.y > b1.y;
     }
 
     private static int nextInt(int min, int max) {
