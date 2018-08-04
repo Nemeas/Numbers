c228b4cc8c15072f3c0779734f640384bd904aef
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-08-04 21:27:39 +0200

    adds function getRandomNumberInRange to support a min, max intervall

--- app/src/main/java/no/nemeas/numbers/Utils.java
+++ app/src/main/java/no/nemeas/numbers/Utils.java
@@ -42,4 +42,14 @@ public class Utils {
     public static String formatSecs(long secs) {
         return (secs < 10) ? "0" + secs : secs + "";
     }
+
+    public static int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
 }
