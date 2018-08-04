9c194e38718c1f670a7154506db70f5685defc44
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-08-04 21:28:37 +0200

    adds a Setup class ho hold a rounds setup, n = number of numbers, min = min number possible to spawn, max = max number possible to spawn

---
+++ app/src/main/java/no/nemeas/numbers/Setup.java
@@ -0,0 +1,13 @@
+package no.nemeas.numbers;
+
+public class Setup {
+    public int n;
+    public int min;
+    public int max;
+
+    public Setup(int n, int min, int max) {
+        this.n = n;
+        this.min = min;
+        this.max = max;
+    }
+}
