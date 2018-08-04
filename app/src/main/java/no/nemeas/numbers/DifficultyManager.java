605aed0efadaa24a0c7b2c1f32397efe56309a23
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-08-04 21:32:30 +0200

    adds a DifficultyManager to manage what kind of Setup the player should have at every stage of the game, missing the implementation yet...

---
+++ app/src/main/java/no/nemeas/numbers/DifficultyManager.java
@@ -0,0 +1,8 @@
+package no.nemeas.numbers;
+
+public class DifficultyManager {
+    public static Setup getSetup(int completedStages, int failedStages, int lvl) {
+        // TODO - implement
+        return new Setup(5, 0, 10);
+    }
+}
