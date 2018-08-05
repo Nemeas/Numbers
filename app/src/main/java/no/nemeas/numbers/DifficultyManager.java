d8be56b2842bfd4215c4e144dc4cfe68f5f8456f
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-08-05 20:16:54 +0200

    trying out a way to set the number of numbers based on the lvl of the player.

--- app/src/main/java/no/nemeas/numbers/DifficultyManager.java
+++ app/src/main/java/no/nemeas/numbers/DifficultyManager.java
@@ -1,8 +1,13 @@
 package no.nemeas.numbers;
 
+import android.util.Log;
+
 public class DifficultyManager {
     public static Setup getSetup(int completedStages, int failedStages, int lvl) {
         // TODO - implement
-        return new Setup(5, 0, 10);
+        Log.d("dif", "c: " + completedStages + "\nf: " + failedStages + "\nlvl: " + lvl);
+        int n = (int)(5 + Math.log(lvl));
+        Log.d("dif", "n: " + n);
+        return new Setup(n, 0, 10);
     }
 }
