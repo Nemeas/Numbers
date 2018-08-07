f8de1f7ef7d338ccee1e9d9d12043e26c5823a32
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-08-07 19:28:06 +0200

    setting the lvl by tapping the splash-screen image

--- app/src/main/java/no/nemeas/numbers/StartScreenActivity.java
+++ app/src/main/java/no/nemeas/numbers/StartScreenActivity.java
@@ -7,14 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class StartScreenActivity extends Activity {
+
+    private ImageView splashImage;
+    private int lvl = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startscreen);
+
+        this.splashImage = (ImageView) findViewById(R.id.splash);
+
+        this.splashImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lvl += 1;
+            }
+        });
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//Set Portrait
 
@@ -23,6 +36,7 @@ public class StartScreenActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getThis(), GameActivity.class);
+                i.putExtra("lvl", lvl);
                 startActivity(i);
             }
         });
