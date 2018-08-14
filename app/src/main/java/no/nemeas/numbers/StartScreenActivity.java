927acb52dd34a51834209fc306c30c90bc0deed1
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-08-14 21:16:13 +0200

    Adds leaderboard to view

--- app/src/main/java/no/nemeas/numbers/StartScreenActivity.java
+++ app/src/main/java/no/nemeas/numbers/StartScreenActivity.java
@@ -32,15 +32,15 @@ import com.google.android.gms.games.Games;
 import com.google.android.gms.games.Player;
 import com.google.android.gms.games.PlayersClient;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class StartScreenActivity extends Activity {
 
-    private ImageView splashImage;
-    private CircleImageView profileImage;
     private int lvl = 1;
+    private static final int RC_LEADERBOARD_UI = 9004;
 
     private Button playButton;
 
@@ -51,9 +51,6 @@ public class StartScreenActivity extends Activity {
 
         setContentView(R.layout.startscreen);
 
-        this.splashImage = (ImageView) findViewById(R.id.splash);
-        this.profileImage = (CircleImageView) findViewById(R.id.profile_image);
-
         GoogleSignInAccount account = (GoogleSignInAccount) getIntent().getParcelableExtra("account");
 
         PlayersClient playersClient = Games.getPlayersClient(this, account);
@@ -62,13 +59,22 @@ public class StartScreenActivity extends Activity {
             @Override
             public void onComplete(@NonNull Task<Player> task) {
                 Player p = task.getResult();
+                CircleImageView profileImage = (CircleImageView) findViewById(R.id.profile_image);
                 new DownloadImageTask(profileImage).execute(p.getIconImageUrl() == null ? null : p.getIconImageUrl());
+            }
+        });
+
+        ((Button) findViewById(R.id.show_leaderboard_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLeaderboard();
             }
         });
 
         Log.d(GameActivity.DEBUG, account.toJson());
 
-        this.splashImage.setOnClickListener(new View.OnClickListener() {
+        ImageView splashImage = (ImageView) findViewById(R.id.splash);
+        splashImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 lvl += 1;
@@ -81,14 +87,25 @@ public class StartScreenActivity extends Activity {
         this.playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getThis(), GameActivity.class);
-                i.putExtra("lvl", lvl);
-                startActivity(i);
+            Intent i = new Intent(getThis(), GameActivity.class);
+            i.putExtra("lvl", lvl);
+            startActivity(i);
             }
         });
     }
 
     private Activity getThis() {
         return this;
+    }
+
+    private void showLeaderboard() {
+        Games.getLeaderboardsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+            .getLeaderboardIntent(getString(R.string.leaderboard_highscore))
+            .addOnSuccessListener(new OnSuccessListener<Intent>() {
+                @Override
+                public void onSuccess(Intent intent) {
+                    startActivityForResult(intent, RC_LEADERBOARD_UI);
+                }
+            });
     }
 }
