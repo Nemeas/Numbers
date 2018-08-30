e5988db5580d9e8d4e54b6da295110d2db394c3e
Tor Christian Sandbakken <tordenskjold_89@hotmail.com>
2018-08-30 22:28:34 +0200

    adds import of context

--- app/src/main/java/no/nemeas/numbers/MainActivity.java
+++ app/src/main/java/no/nemeas/numbers/MainActivity.java
@@ -1,6 +1,7 @@
 package no.nemeas.numbers;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
